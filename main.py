8d815acbf9ec084cc1f55ade1ccc6eebd70be48f
sephib <js.berry@gmail.com>
2018-06-28 00:28:41 +0300

    Added country and year

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ import pandas as pd
 data_dir ='.'
 
 
+def read_squad_to_df(l:list):
+    print('process Squad')
+    df_all = pd.DataFrame()
+    for team in l:
+        print(team)
+        df = pd.read_fwf(team, skiprows=4,
+                         names=['num_in_country', 'position', 'name', 'sepe', 'num_in_group', 'team'])
+        df['country'] = team.parts[-1].split('-')[1].split('.')[0]
+        df_all = pd.concat([df_all, df])
+    df_all['year'] = l[0].parts[0].split('--')[0]
+    return df_all
+
+
 def main():
     df_all = pd.DataFrame()
     df = pd.DataFrame()
@@ -13,13 +26,9 @@ def main():
         if len(l) > 1:
             if l[-2].parent != l[-1].parent:
                 j = l.pop()
-                print(f'process: {l}')
                 if 'squads' in l[0].parts:
-                    print('process Squad')
-                    df = pd.read_fwf(l[0], skiprows=4,
-                                     names=['num_in_country', 'position', 'name', 'sepe', 'num_in_group', 'team'])
-                    df['country'] = l[0].parts[-1].split('-')[1].split('.')[0]
-                print(f'--------------')
+                    # print(f'process: {l}')
+                    df = read_squad_to_df(l)
                 l = []
                 l.append(j)
                 if not df.empty:
@@ -30,7 +39,7 @@ def main():
     print(f'len {df_all.shape}')
     print(f'{df_all.info()}')
     print(f'{df_all.head()}')
-    df_all.to_csv('df_all.csv')
+    df_all.to_csv('df_all.csv', index = False)
 
 
 
