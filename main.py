30606546212858b9e5268fb1241bd1ca711135cd
sephib <js.berry@gmail.com>
2018-07-01 01:17:31 +0300

    improve cleaning DF

--- main.py
+++ main.py
@@ -38,13 +38,17 @@ def main():
                 pass
     #Clean dataframe
     mask = df_all.num_in_country.isnull()
-    df_all = df_all[~mask]
+    df_all = df_all[~mask].reset_index(drop=True)
+    df_all['num_in_group_team'] = df_all.num_in_group + df_all.team.fillna('')
+    dft = df_all['num_in_group_team'].str.split(',', n=1, expand=True)
+    df_all['num_in_group'], df_all['team'] = dft[0], dft[1]
+    df_all = df_all.drop(['num_in_group_team'], axis=1)
     print(f'len {df_all.shape}')
     print(f'{df_all.info()}')
     print(f'{df_all.head()}')
-    df_all.to_csv('df_all.csv', index = False)
-
+    df_all.to_csv('df_all.csv', index=False)
 
 
 if __name__ == '__main__':
     main()
+    print('Done')
